9526bbc2e76bdc3ff752f2b25cf32ad430b504bd
Baxter Crabtree <baxter.crabtree@gmail.com>
2018-03-25 23:30:13 -0400

    adding callbacks for the window to windows.

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,22 +1,57 @@
 #include <windows.h>
 
+LRESULT CALLBACK 
+MainWindowCallback(HWND   Window,
+                   UINT   Message,
+                   WPARAM WParam,
+                   LPARAM LParam)
+{
+  switch(Message)
+  {
+    case WM_SIZE:
+    {
+      OutputDebugStringA("WM_SIZE\n");
+    }break;
+
+    case WM_DESTROY:
+    {
+      OutputDebugStringA("WM_DESTROY\n");
+    }break;
+   
+    case WM_CLOSE:
+    {
+      OutputDebugStringA("WM_CLOSE\n");
+    }break;
+
+    case WM_ACTIVATEAPP:
+    {
+      OutputDebugStringA("WM_ACTIVATEAPP\n");
+    }break;
+
+    default:
+    {
+//      OutputDebugStringA("default\n");
+    }break;
+  } 
+}
+
 /*WinMain is what we need to call to get the OS talking to the application. 
 Very first thing that needs to be done*/
 int CALLBACK 
-WinMain(HINSTANCE hInstance,
-        HINSTANCE hPrevInstance,
-        LPSTR     lpCmdLine,
-        int       nCmdShow
-)
+WinMain(HINSTANCE Instance,
+        HINSTANCE PrevInstance,
+        LPSTR     CommandLine,
+        int       ShowCode)
 {
   //Info on WNDCLASS at https://msdn.microsoft.com/en-us/library/windows/desktop/ms633576(v=vs.85).aspx
   WNDCLASS WindowClass = {0};
 
   //TODO: Check that HREDRAW and VREDRAW still matter to us.
   WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW; //Allocates a unique device context for each window in the class.
+  // This will handle all of the messages coming from Windows.
   WindowClass.lpfnWndProc = ;
   WindowClass.hInstance = hInstance;
-//  HICON     hIcon; Will use this for game icon eventually.
+  //HICON     hIcon; Will use this for game icon eventually.
   WindowClass.lpszClassName = "FictionalRobotWindowClass";
 
   return(0);
